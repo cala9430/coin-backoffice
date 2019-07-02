561848cda2dcda163ef6d1d545970a10e24c0809
Daniel Calabuig <calabuig.daniel@gmail.com>
2019-07-01 22:11:25 -0300

    Create SpringBoot project
    Create Angular project

---
+++ src/main/java/com/example/coinbackoffice/CoinBackofficeApplication.java
@@ -0,0 +1,13 @@
+package com.example.coinbackoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinBackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinBackofficeApplication.class, args);
+	}
+
+}
