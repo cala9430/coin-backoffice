d505a444e3a1d0a9f5c851ac2eb36668245565e9
Daniel Calabuig <calabuig.daniel@gmail.com>
2019-07-03 22:12:51 -0300

    Try to connect to local MongoDB

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # coin-backoffice
+
+####Download image from docker-hub
+`docker pull cala9430/coin-backoffice:<version>`
+
+####Run image on container
+`docker run --rm -p 8080:8080 cala9430/coin-backoffice:5`
--- src/main/java/com/example/coinbackoffice/config/MongoConfig.java
+++ src/main/java/com/example/coinbackoffice/config/MongoConfig.java
@@ -16,6 +16,6 @@ public class MongoConfig extends AbstractMongoConfiguration {
 
     @Override
     public MongoClient mongoClient() {
-        return new MongoClient("127.0.0.1", 27017);
+        return new MongoClient("host.docker.internal", 27017);
     }
 }
